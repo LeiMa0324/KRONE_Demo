eb18007040cdb6bdef9934f2e83b0ca993aaf82c
atassiad <atassiadamis@gmail.com>
2025-06-02 09:09:04 -0400

    Added routing to the navbar, yeehaw

--- krone_v2_react_tailwind_shadcn/src/components/navbar.tsx
+++ krone_v2_react_tailwind_shadcn/src/components/navbar.tsx
@@ -1,19 +1,32 @@
 import { Button } from "@/components/ui/button";
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import { Link } from "react-router-dom"
 
 export const NavBar = () => {
     return (
         <nav className="fixed w-full z-40 transition-all duration-300 bg-amber-600">
             <div className="container flex items-left gap-4 px-3 py-3 items-center">
-                <Avatar className="size-12">
-                    <AvatarImage src="/cropped_wpi_logo.png" />
-                    <AvatarFallback>WPI</AvatarFallback>
-                </Avatar>
-                <a className="font-bold text-3xl text-gray-100">KRONE</a>
-                <Button>File Upload</Button>
-                <Button>Visualize Tree</Button>
-                <Button>Log Table</Button>
-                <Button>About</Button>
+                <Link to="/">
+                    <Avatar className="size-12">
+                        <AvatarImage src="/cropped_wpi_logo.png" />
+                        <AvatarFallback>WPI</AvatarFallback>
+                    </Avatar>
+                </Link>
+                <Link to="/">
+                    <a className="font-bold text-3xl text-gray-100">KRONE</a>
+                </Link>
+                <Link to="/file-upload">
+                    <Button>File Upload</Button>
+                </Link>
+                <Link to="/visualize-tree">
+                    <Button>Visualize Tree</Button>
+                </Link>
+                <Link to="/log-table">
+                    <Button>Log Table</Button>
+                </Link>
+                <Link to="/about">
+                    <Button>About</Button>
+                </Link>
             </div>
         </nav>
     );
