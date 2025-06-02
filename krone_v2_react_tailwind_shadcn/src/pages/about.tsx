5bf3a6a7d5bfab8547f1166972b1d40a5e421218
atassiad <atassiadamis@gmail.com>
2025-06-02 09:04:13 -0400

    Added routing for each seperate page section

---
+++ krone_v2_react_tailwind_shadcn/src/pages/about.tsx
@@ -0,0 +1,6 @@
+
+export const About = () => {
+    return (
+        <div className="text-9xl text-amber-900"> This will be the about page </div>
+    )
+}
--- krone_v2_react_tailwind_shadcn/src/pages/home.tsx
+++ krone_v2_react_tailwind_shadcn/src/pages/home.tsx
@@ -1,12 +1,10 @@
 import { WPIBackground } from "@/components/WPIbackground";
-import { NavBar } from "@/components/navbar"
 import { HeroSection } from "@/components/hero"
 
 export const Home = () => {
     return (
         <div className="min-h-screen text-foreground overflow-x-hidden">
             <WPIBackground></WPIBackground>
-            <NavBar></NavBar>
             <main> 
                 <HeroSection></HeroSection>
             </main>
