99c03a3a269da728e4c1343d1885ffa6b95da91b
atassiad <atassiadamis@gmail.com>
2025-06-03 09:26:38 -0400

    Fixed Suhani's blunder

--- krone_v2_react_tailwind_shadcn/src/components/hero.tsx
+++ krone_v2_react_tailwind_shadcn/src/components/hero.tsx
@@ -7,15 +7,26 @@ export const HeroSection = () => {
             id="hero"
             className="relative min-h-screen flex flex-col items-center justify-center px-4"
         >
-            <div className="absolute inset-0 bg-black opacity-55 z-0 pointer-events-none"/>
-            <div className="relative text-center flex flex-col items-center z-10">
-                <h1 className="text-6xl md:text-8xl font-extrabold tracking-tight text-white mb-6">
-                    Introducing <span className="text-primary ml-4 text-white">Krone</span>
-                </h1>
-                <p className="text-2xl md:text-3xl text-white max-w-3xl mx-auto font-bold mb-10">
-                    The newest state-of-the-art machine learning powered pinpoint log anomaly detector
-                </p>
-                <Button className="text-xl px-10 py-6 rounded-lg"> Try Out Krone </Button>
+            <div className="relative text-center flex flex-col items-center">
+                <div className="inline-block relative">
+                    <div className="absolute inset-0 translate-x-4 translate-y-4 bg-white z-0"></div>
+                    <div className="relative bg-WPIRed shadow-2xl px-14 py-8 z-10">
+                        <h1 className="text-5xl md:text-7xl font-extrabold tracking-tight text-white">
+                            <span> Introducing </span>
+                            <span className="ml-4 text-white"> Krone </span>
+                        </h1>
+                    </div>
+                </div>
+                <div className="inline-block relative mt-8">
+                    <p className="text-2xl md:text-3xl text-white max-w-3xl mx-auto font-bold" style={{ textShadow: "6px 6px 16px black" }}>
+                        The newest state-of-the-art machine learning powered pinpoint log anomaly detector
+                    </p>
+                </div>
+                <div className="pt-8">
+                    <Link to="/file-upload">
+                        <Button className="text-xl px-10 py-6 rounded-lg"> Try Out Krone </Button>
+                    </Link>
+                </div>
             </div>
         </section>
     );
