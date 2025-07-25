da59dabbf48af8666fbd6dde010109a08e0618d7
EShanbaum <shanbaumethan@gmail.com>
2025-07-25 11:22:07 -0400

    Removed console log

--- src/pages/knowledge_base_viz.tsx
+++ src/pages/knowledge_base_viz.tsx
@@ -230,12 +230,10 @@ export const KnowledgeBaseViz = () => {
 
     // When a node is clicked it query's the selected node and displays sidebar with that nodes children sequences
     const handleNodeClick = (node: { data: TreeNode }) => {
-        console.log("Clicked node:", node);
         if (node.data?.name) {
             setSelectedQuery(node.data.name);
             setShowSidebar(true);
             setSearchLogKey("");
-            console.log("Test")
             window.history.replaceState({}, document.title, window.location.pathname);
         }
     };
